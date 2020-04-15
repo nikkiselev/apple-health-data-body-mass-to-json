92833e6eaa6d254cc06dcb20b72f0ce3432037f2
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2020-04-15 17:41:16 +0400

    Read the given file and print the content

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export default () => [{}]
+import { promises as fs } from 'fs'
+import path from 'path'
+
+export default async (givenPath: string) => {
+  const content = await fs.readFile(path.join(__dirname, givenPath), 'utf8')
+  return content
+}
