1271a2324ac415ab439d11148334116ca4264c89
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2020-04-16 12:16:00 +0400

    Add data parsing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,23 @@ import { parseStringPromise } from 'xml2js'
 export default async (givenPath: string) => {
   const content = await fs.readFile(path.join(__dirname, givenPath), 'utf8')
 
-  const parsed = await parseStringPromise(content)
+  const parsed = await parseStringPromise(content, { ignoreAttrs: true })
 
-  return parsed
+  const entries: any[] =
+    parsed?.ClinicalDocument?.component[0]?.section[0].entry
+
+  const data: any = []
+
+  entries.forEach((entry: any) => {
+    entry?.organizer[0]?.component.forEach((component: any) => {
+      if (
+        component?.observation[0]?.text[0]?.type[0] ===
+        'HKQuantityTypeIdentifierBodyMass'
+      ) {
+        data.push({ weight: component?.observation[0]?.text[0]?.value[0] })
+      }
+    })
+  })
+
+  return data
 }
