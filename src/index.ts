e16cb3f8757876e2af16abde8e99d735831b45a5
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2020-04-16 12:18:18 +0400

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,9 @@ export default async (givenPath: string) => {
 
   entries.forEach((entry: any) => {
     entry?.organizer[0]?.component.forEach((component: any) => {
-      if (
-        component?.observation[0]?.text[0]?.type[0] ===
-        'HKQuantityTypeIdentifierBodyMass'
-      ) {
-        data.push({ weight: component?.observation[0]?.text[0]?.value[0] })
+      const text: any = component?.observation[0]?.text[0]
+      if (text.type[0] === 'HKQuantityTypeIdentifierBodyMass') {
+        data.push({ weight: text.value[0] })
       }
     })
   })
