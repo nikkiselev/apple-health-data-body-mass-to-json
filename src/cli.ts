6a2099c804da817a207768af9066f73801a13b6d
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2020-04-16 16:05:08 +0400

    Write output to a file

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 import minimist from 'minimist'
 import getBodyMassFromFile from './getBodyMassFromFile'
+import { promises as fs } from 'fs'
+import path from 'path'
 
 const args = minimist(process.argv.slice(2))
 
-;(async () => console.log(await getBodyMassFromFile(args['in'])))()
+;(async () => {
+  const data = JSON.stringify(await getBodyMassFromFile(args['in']))
+  fs.writeFile(path.join(__dirname, '../', args['out']), data, 'utf8')
+})()
