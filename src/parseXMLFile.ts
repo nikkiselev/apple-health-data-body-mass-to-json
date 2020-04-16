d32b01b2888e90899382f105846e33423a4b8fc9
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2020-04-16 16:08:11 +0400

    Look for a file in a root dir

--- src/parseXMLFile.ts
+++ src/parseXMLFile.ts
@@ -6,7 +6,10 @@ import { parseStringPromise } from 'xml2js'
  * Parse an XML file on the given path to an object
  */
 export default async (filePath: string) => {
-  const content = await fs.readFile(path.join(__dirname, filePath), 'utf8')
+  const content = await fs.readFile(
+    path.join(__dirname, '../', filePath),
+    'utf8'
+  )
 
   return parseStringPromise(content)
 }
